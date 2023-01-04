23fb8897a4280609a54a2cef6d864726f7744cda
Vyacheslav Yudin <valentingullll@gmail.com>
2023-01-05 02:26:48 +0500

    Change project

--- backend/main.py
+++ backend/main.py
@@ -10,4 +10,4 @@ async def index():
 
 
 if __name__ == '__main__':
-    uvicorn.run(app='main:application', host='0.0.0.0', port=49785, reload=True)
+    uvicorn.run(app='main:application', host='0.0.0.0', port=49567, reload=True)
