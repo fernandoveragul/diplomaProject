01d1931baf68de3d4b559ced7ef39446a80fa81d
Vyacheslav Yudin <valentingullll@gmail.com>
2023-01-05 03:33:29 +0500

    Chachge architech

--- backend/main.py
+++ backend/main.py
@@ -9,5 +9,9 @@ async def index():
     return {b'HELLO WORLD': 2022}
 
 
+def run():
+    uvicorn.run(app='backend.main:application', host='0.0.0.0', port=49567, reload=True)
+
+
 if __name__ == '__main__':
-    uvicorn.run(app='main:application', host='0.0.0.0', port=49567, reload=True)
+    run()
