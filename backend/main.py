bec0c110c80ce663f640d0ecebd98ad3d4c6ed76
fernandoveragul <valentingullll@gmail.com>
2023-01-06 21:02:58 +0500

    Create Dockerfile

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ async def index():
 
 
 def run():
-    uvicorn.run(app='backend.main:application', host='0.0.0.0', port=49567, reload=True)
+    uvicorn.run(app='main:application', host='0.0.0.0', port=48569, reload=True)
 
 
 if __name__ == '__main__':
