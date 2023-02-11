76143b1a93bc44c092e0807e94bb4beb3a769248
fernandoveragul <valentingullll@gmail.com>
2023-02-11 14:40:11 +0500

    Message

--- services/backend/src/database.py
+++ services/backend/src/database.py
@@ -4,14 +4,14 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, AsyncEngin
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from config import DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME
-
-DB_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?async_fallback=True"
+from src.config import DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME
+DB_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+print(f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@db:{DB_PORT}/{DB_NAME}")
 
 Base = declarative_base()
 
-async_engine: AsyncEngine = create_async_engine(DB_URL)
-async_session_maker = sessionmaker(async_engine, class_=AsyncSession, expire_on_commit=False)
+engine = create_async_engine(DB_URL)
+async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
--- services/backend/src/news/router.py
+++ services/backend/src/news/router.py
@@ -1,27 +1,36 @@
+from typing import Any
 from uuid import UUID
 
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, Result, insert, update, delete
 
+from src.database import get_async_session
+from src.news.models import posts
 from src.news.schemas import Post
 
 news_router = APIRouter(prefix="/news")
 
 
 @news_router.get(path="/all", status_code=status.HTTP_200_OK, response_model=list[Post])
-async def get_all_posts() -> list[Post]:
-    ...
+async def get_all_posts(session: AsyncSession = Depends(get_async_session)) -> Result[Any]:
+    query = select(posts)
+    return await session.execute(query)
 
 
 @news_router.post(path="/create", status_code=status.HTTP_201_CREATED)
-async def create_post(post: Post):
-    ...
+async def create_post(post: Post, session: AsyncSession = Depends(get_async_session)) -> None:
+    await session.execute(insert(posts).values(**post))
+    await session.commit()
 
 
 @news_router.put(path="/update", status_code=status.HTTP_202_ACCEPTED)
-async def update_post(uuid_post: UUID):
-    ...
+async def update_post(replacement: Post, uuid_post: UUID, session: AsyncSession = Depends(get_async_session)):
+    await session.execute(update(posts).values(**replacement).where(posts.c.uuid_post == uuid_post))
+    await session.commit()
 
 
 @news_router.delete(path="/delete", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_post(uuid_post: UUID):
-    ...
+async def delete_post(uuid_post: UUID, session: AsyncSession = Depends(get_async_session)):
+    await session.execute(delete(posts).where(posts.c.uuid_post == uuid_post))
+    await session.commit()
