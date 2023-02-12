ef2bcbff53b14af98ed9200618748d4fafee24cf
fernandoveragul <valentingullll@gmail.com>
2023-02-12 23:34:32 +0500

    Create dependencies

--- services/backend/src/auth/dependencies.py
+++ services/backend/src/auth/dependencies.py
@@ -1,8 +1,12 @@
-from fastapi import Depends
+from datetime import timedelta, datetime
+
+from fastapi import Depends, HTTPException, status
 from fastapi.security.oauth2 import OAuth2PasswordBearer
+from jose import jwt, JWTError
 from passlib.context import CryptContext
 
-from src.users.schemas import User
+from src.auth.schemas import TokenData
+from src.config import SECRET_KEY, ALGO
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
@@ -22,15 +26,37 @@ class TVPassword:
         return TVPassword.CONTEXT.hash(password)
 
     @classmethod
-    async def create_token(cls, *, data: dict):
-        ...
+    async def create_token(cls, *, data: dict, expires_delta: timedelta | None = None):
+        credentials_exception = HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+        to_encode = data.copy()
+        if expires_delta:
+            expire = datetime.utcnow() + expires_delta
+        else:
+            expire = datetime.utcnow() + timedelta(minutes=15)
+        to_encode.update({"exp": expire})
+        try:
+            encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGO)
+        except JWTError:
+            raise credentials_exception
+        return encoded_jwt
 
-
-def fake_decode_token(token):
-    return User(
-        nickname_user=token + "fakedecoded", email_user="john@example.com")
-
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    user = fake_decode_token(token)
-    return user
+    @classmethod
+    async def decode_jwt(cls, *, token):
+        credentials_exception = HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+        try:
+            payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGO])
+            username: str = payload.get("sub")
+            if username is None:
+                raise credentials_exception
+            token_data = TokenData(username=username)
+        except JWTError:
+            raise credentials_exception
+        return token_data
--- services/backend/src/auth/router.py
+++ services/backend/src/auth/router.py
@@ -1,22 +1,20 @@
 from fastapi import APIRouter, status, Depends
 
-from src.auth.dependencies import get_current_user
-from src.auth.schemas import AccessToken
 from src.users.schemas import User
 
 auth_router = APIRouter(prefix="/auth")
 
 
 @auth_router.post(path="/sign-in", status_code=status.HTTP_202_ACCEPTED)
-async def sing_in(token: AccessToken):
+async def sing_in():
     ...
 
 
-@auth_router.post(path="/sign-up", status_code=200)
+@auth_router.post(path="/sign-up", status_code=status.HTTP_201_CREATED)
 async def sign_up():
     ...
 
 
-@auth_router.get("/me")
-async def read_users_me(current_user: User = Depends(get_current_user)):
-    return current_user
+@auth_router.get("/me", status_code=status.HTTP_202_ACCEPTED)
+async def read_users_me(current_user: User):
+    ...
